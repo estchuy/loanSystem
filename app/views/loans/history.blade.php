cf74a7a8b4d6402b83118cc8cc131cc246a9c6fe
Estuardo Chuy <estchuy@hotmail.com>
2016-05-01 02:11:37 -0600

    Cambia de vista de historial de pagos

--- app/views/loans/history.blade.php
+++ app/views/loans/history.blade.php
@@ -1,85 +1,66 @@
 @section('content')
-<section class="wrapper">
-	<div class="row">
-		<div class="col-md-3"></div>
-	  	<div class="col-md-6">
-	      <div class="content-panel">
-          	<div class="form-group">
-		       	<label class="col-sm-5 control-label"><h4><i class="fa fa-money"></i> Historico de Pagos</h4></label>
-	            <div class="col-sm-12">
-	             	<hr>
-	            </div>
-			</div>
-			@foreach($history as @data)
-				<table class="table table-striped table-advance table-hover mt">
-	              <thead>
-		              <tr>
-		                  <th colspan="4" align="center"><strong>Pago # {{$data->paymentNumber}}</strong></th>
-		              </tr>
-	              </thead>
-	              <thead>
-		              <tr>
-		              	  <th>#</th>
-		                  <th><i class="fa fa-user"></i> Cliente</th>
-		                  <th><i class="fa fa-sort-numeric-asc"></i> Cuota</th>
-		                  <th><i class="fa fa-money"></i> Monto</th>
-		              </tr>
-	              </thead>
-	              <tbody>
-	              	<?php 
-	              		$totalPay = 0;
-	              		$i = 1;
-	              		$loans = PayLoan::gethistory($data->paymentNumber);
-	              	?>
-	              @foreach($loans as $loan)	
-	              	<tr>
-	              		<td>{{$i}}</td>
-		                <td>{{$loan->name}}</td>
-		                <td>{{$loan->period_id."/".$loan->total_of_periods}}</td>
-		                <td>Q{{number_format($loan->monthly_payment, 2, '.', ',')}}</td>
-	              	</tr>
-	              	<?php
-	              		$totalPay = $totalPay + $loan->monthly_payment;
-	              		$i++;
-	              	?>
-	              	@endforeach
-	           	</tbody>
-	           	<thead>
-	           		<tr>
-	           			<th colspan="3"><strong>Total</strong></th>
-	           			<th><strong>Q{{number_format($totalPay, 2, '.', ',')}}</strong></th>
-	           		</tr>
-	           	</thead>
-	          </table>
-	         @endforeach
-	      </div><!-- /content-panel -->
-	  	</div><!-- /col-md-6 -->
-	  	<div class="col-md-3"></div>
-	</div><!-- /row -->
-	<div class="row mt">
-		<div class="col-md-3"></div>
-		<div class="col-md-6">
-			
-				<div class="col-md-2">
-					<form method="post" id="download" action="/reporte/download">
-						<input type="hidden" id="action" name="action" value="detallado">
-						<button class="btn btn-warning" type="submit" form="download">
-							<i class="fa fa-download"></i>
-							PDF
-						</button>
-					</form>
-				</div>
-				<div class="col-md-2"></div>
-				<div class="col-md-2">
-					<input type="hidden" id="action" name="action" value="detallado">
-					<button class="btn btn-success" type="submit" form="aplicarPago">
-						<i class="fa fa-sign-out"></i>
-						Aplicar Pago
-					</button>
-				</div>
-			
-		</div>
-		<div class="col-md-3"></div>
+<div class="row">
+	<div class="col-lg-3"></div>
+	<div class="col-lg-6">
+		<div class="panel-group" id="accordion-test-2"> 
+        	<h4 class="m-t-0 header-title"><i class="fa fa-history"></i><b> Historial de Pagos</b></h4>
+        	<?php $i = 1;?>
+        	@foreach($history as $data)
+        		<?php
+        			$totalPay = PayLoan::getTotalPay($data->paymentNumber);
+        		?>
+        		<div class="panel panel-default panel-color">
+		            <div class="panel-heading"> 
+		                <h4 class="panel-title"> 
+		                    <a data-toggle="collapse" data-parent="#accordion-test-2" href="#collapseLoan_{{$i}}" aria-expanded="false" class="collapsed">
+		                        <i class="fa fa-shopping-bag"></i> Pago 
+		                        <i class="fa fa-hashtag"></i> {{$data->paymentNumber}} - 
+		                        <b>Q {{number_format($totalPay, 2, '.', ',')}}</b> - 
+		                        Fecha de Pago {{substr($data->created_at, 0, 10);}}
+		                    </a> 
+		                </h4> 
+            		</div> 
+		            <div id="collapseLoan_{{$i}}" class="panel-collapse collapse"> 
+		                <div class="panel-body">
+		                  <table class="table-bordered  table table-hover" >
+		                    <thead>
+		                      <tr style="color: #eeeeee;background-color: #517fa4">
+		                     	<th class="text-center"><i class="fa fa-hashtag"></i></th>
+			                  	<th><i class="fa fa-user"></i> Cliente</th>
+			                  	<th class="text-center"><i class="fa fa-sort-numeric-asc"></i> Cuota</th>
+			                  	<th><i class="fa fa-money"></i> Monto</th>
+		                      </tr>
+		                    </thead>
+		                    <tbody>
+		                    	<?php 
+				              		$ihi = 1;
+				              		$loans = PayLoan::gethistory($data->paymentNumber);
+				              	?>
+				              	@foreach($loans as $loan)
+			                        <tr>
+					              		<td class="text-center">{{$ihi}}</td>
+						                <td>{{$loan->name}}</td>
+						                <td class="text-center">{{$loan->period_id."/".$loan->total_of_periods}}</td>
+						                <td>Q{{number_format($loan->monthly_payment, 2, '.', ',')}}</td>
+					              	</tr>
+					              	<?php
+					              		$ihi++;
+					              	?> 
+					           	@endforeach
+		                    </tbody>
+		                    <thead>
+				           		<tr>
+				           			<th colspan="3"><strong>Total</strong></th>
+				           			<th><strong>Q{{number_format($totalPay, 2, '.', ',')}}</strong></th>
+				           		</tr>
+				           	</thead>
+		                  </table>
+		                </div> 
+		            </div> 
+	          	</div>
+	          	<?php $i++;?>
+        	@endforeach
+        </div>
 	</div>
-</section>
+</div>
 @stop
