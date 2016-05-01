b4bf38dfd81bb9d429a2fdfb243ca1a9fe94a229
Estuardo Chuy <estchuy@hotmail.com>
2016-05-01 15:26:16 -0600

    cambio en migración con nombre de tabla

--- app/database/migrations/2016_03_28_142809_dbCreation.php
+++ app/database/migrations/2016_03_28_142809_dbCreation.php
@@ -12,7 +12,7 @@ class DbCreation extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('user', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('username');
