8479422b3a8dff380e5060fb77c5d99a12c003a6
Estuardo Chuy <estchuy@hotmail.com>
2016-05-01 02:10:01 -0600

    nuevos métodos

--- app/models/PayLoan.php
+++ app/models/PayLoan.php
@@ -12,11 +12,30 @@ class PayLoan extends Eloquent {
 
     public static function gethistory($paymentNumber){
     	$history = PayLoan::select('loans.*', 'clients.name')
-    	->join('loans', 'loans.id', '=', 'payloans.loan_id')
+    	->join('loans', 'loans.id', '=', 'pay_loans.loan_id')
     	->join('clients', 'clients.id', '=', 'loans.client_id')
     	->where('paymentNumber', $paymentNumber)
     	->get();
     	
     	return $history;
+    }
+    public static function getTotalPay($paymentNumber){
+        $pagosDetallado = PayLoan::where('paymentNumber', $paymentNumber)
+        ->lists('loan_id');
+
+        $totalPay = Loan::select(DB::raw('sum(monthly_payment) as totalPay'))
+        ->whereIn('id', $pagosDetallado)
+        ->first();
+        
+        return $totalPay->totalPay;
+    }
+    public static function getLastDayPaid(){
+        $lastdayPay = PayLoan::orderBy('id', 'desc')
+        ->first();
+        $string = "";
+        if(count($lastdayPay) > 0){
+            $string = '<span class="label label-danger">Ultima Fecha de Pago Aplicado '.substr($lastdayPay->created_at, 0, 10).'</span>';
+        }
+        return $string;
     }
 }
