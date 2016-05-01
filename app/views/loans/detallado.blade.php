ef6b889cc2e90ac9a6ea89b57d58ba0e228a4da3
Estuardo Chuy <estchuy@hotmail.com>
2016-04-30 22:34:16 -0600

    Reporte detallado

--- app/views/loans/detallado.blade.php
+++ app/views/loans/detallado.blade.php
@@ -1,109 +1,57 @@
 @section('content')
-<script type="text/javascript">
-  function selectAll(){
-    i = 0;
-    while(document.getElementById(i)){
-    	if(document.getElementById('select').value == 'checked'){
-    		document.getElementById(i).checked = false;
-    	}else{
-    		document.getElementById(i).checked = true;
-    	}
-    	i++;
-    }
-    if(document.getElementById('select').value == 'checked'){
-		document.getElementById('select').value = "";
-	}else{
-		document.getElementById('select').value = "checked";
-	}
-  }
-  function genPDF(){
-  	document.getElementById("download").submit();
-  }
-</script>
-<section class="wrapper">
-	<form method="post" id="aplicarPago" action="/reporte/apply">
-		<div class="row">
-		<div class="col-md-3"></div>
-		  <div class="col-md-6">
-		      <div class="content-panel">
-		          <table class="table table-striped table-advance table-hover">
-		          	<div class="form-group">
-				       	<label class="col-sm-5 control-label"><h4><i class="fa fa-users"></i> Reporte Detallado</h4></label>
-			            <div class="col-sm-12">
-			             	<hr>
-			            </div>
-					</div>
-		              <thead>
-		              <tr>
-		                  <th><i class="fa fa-user"></i> Cliente</th>
-		                  <th><i class="fa fa-sort-numeric-asc"></i> Cuota</th>
-		                  <th><i class="fa fa-money"></i> Monto</th>
-		                  <th>
-		                  	<a onclick="javascript:selectAll();"><i class="fa fa-check-square"></i></a>
-		                  	<input type="hidden" id="select" name="select" value="checked" />
-		                  </th>
-		              </tr>
-		              </thead>
-		              <tbody>
-		              	<?php 
-		              		$totalPay = 0;
-		              		$i = 0;
-		              	?>
-		              @foreach($loans as $loan)	
-		              	<tr>
-			                <td>{{$loan->name}}</td>
-			                <td>{{$loan->period_id."/".$loan->total_of_periods}}</td>
-			                <td>Q{{number_format($loan->monthly_payment, 2, '.', ',')}}</td>
-			                <td>
-			                	<input type="checkbox" id="{{$i}}"  class="checked" checked="checked" value="1">
-			                	<input type="hidden" id="loan_id_{{$i}}" value="{{$loan->id}}" name="loan_id_{{$i}}" />
-			                </td>
-		              	</tr>
-		              	<?php
-		              		$totalPay = $totalPay + $loan->monthly_payment;
-		              		$i++;
-		              	?>
-		              	@endforeach
-		           	</tbody>
-		           	<thead>
-		           		<tr>
-		           			<th colspan="2"><strong>Total</strong></th>
-		           			<th><strong>Q{{number_format($totalPay, 2, '.', ',')}}</strong></th>
-		           			<th>
-		           				<input type="hidden" id="total_Loans" value="{{$i}}" name="total_Loans" />
-		           			</th>
-		           		</tr>
-		           	</thead>
-		          </table>
-		      </div><!-- /content-panel -->
-		  </div><!-- /col-md-6 -->
-		  <div class="col-md-3"></div>
-		</div><!-- /row -->
-		<div class="row mt">
-			<div class="col-md-3"></div>
-			<div class="col-md-6">
-				
-					<div class="col-md-2">
-						<button class="btn btn-warning" type="button" onclick="javascript:genPDF();">
-							<i class="fa fa-download"></i>
-							PDF
-						</button>
-					</div>
-					<div class="col-md-2"></div>
-					<div class="col-md-2">
-						<input type="hidden" id="action" name="action" value="detallado">
-						<button class="btn btn-success" type="submit" form="aplicarPago">
-							<i class="fa fa-sign-out"></i>
-							Aplicar Pago
-						</button>
-					</div>
-				
-			</div>
-			<div class="col-md-3"></div>
-		</div>
-	</form>
-	<form method="post" id="download" action="/reporte/download">
-		<input type="hidden" id="action" name="action" value="detallado">
-	</form>
-</section>
+<title> - Reporte Detallado</title>
+<div class="row">
+	<div class="col-sm-2"></div>
+    <div class="col-sm-8">
+        <div class="card-box table-responsive">
+            <h4 class="m-t-0 header-title"><i class="fa fa-list-alt "></i> <b>  Reporte Detallado </b></h4>
+        	 <p class="text-muted font-13 m-b-30">
+            </p>
+            <form method="post" id="aplicarPago" action="/reporte/apply">
+	            <table id="datatable-buttons" class="table table-striped table-bordered">
+	                <thead>
+	                    <tr>
+			            	<th><i class="fa fa-user"></i> Cliente</th>
+			               	<th><i class="fa fa-sort-numeric-asc"></i> Cuota</th>
+			              	<th><i class="fa fa-money"></i> Monto</th>
+			              </tr>
+	                </thead>
+	                <tbody>
+			              	<?php 
+			              		$totalPay = 0;
+			              		$i = 0;
+			              	?>
+			              @foreach($loans as $loan)	
+			              	<tr>
+				                <td>{{$loan->name}}</td>
+				                <td>{{$loan->period_id."/".$loan->total_of_periods}}</td>
+				                <td>Q{{number_format($loan->monthly_payment, 2, '.', ',')}}</td>
+			              	</tr>
+			              	<?php
+			              		$totalPay = $totalPay + $loan->monthly_payment;
+			              		$i++;
+			              	?>
+			              	@endforeach
+			           	</tbody>
+			           	<thead>
+			           		<tr>
+			           			<th colspan="2"><strong>Total</strong></th>
+			           			<th>
+			           				<strong>Q{{number_format($totalPay, 2, '.', ',')}}</strong>
+			           				<input type="hidden" id="total_Loans" value="{{$i}}" name="total_Loans" />
+			           			</th>
+			           		</tr>
+			           	</thead>
+		        </table>
+		        <div>
+		        	<input type="hidden" id="action" name="action" value="detallado">
+					<button class="btn btn-success btn-custom waves-effect waves-light" type="submit" form="aplicarPago">
+						<i class="fa fa-sign-out"></i>
+						Aplicar Pago
+					</button>
+		       	</div>
+		   	</form>
+	    </div>
+	</div>
+</div>
 @stop
