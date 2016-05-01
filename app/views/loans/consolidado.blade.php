ce018e6c557dd9cb12dd13a3a4b867834769cd97
Estuardo Chuy <estchuy@hotmail.com>
2016-04-30 22:34:48 -0600

    new report

--- app/views/loans/consolidado.blade.php
+++ app/views/loans/consolidado.blade.php
@@ -1,58 +1,36 @@
 @section('content')
-<section class="wrapper">
-	<div class="row">
-	<div class="col-md-4"></div>	
-	  <div class="col-md-4">
-	      <div class="content-panel">
-	          <table class="table table-striped table-advance table-hover">
-	          	<div class="form-group">
-			       	<label class="control-label"><h4><i class="fa fa-users"></i> Reporte Consolidado</h4></label>
-		            <div class="col-sm-12">
-		             	<hr>
-		            </div>
+<div class="row">
+	<div class="col-sm-4"></div>
+	<div class="col-sm-4">
+		<div class="card-box table-responsive">
+			<table class="table-bordered  table table-hover" >
+				<div class="form-group">
+			       	<label class="control-label"><h4><i class="fa fa-list-alt "></i> Reporte Consolidado</h4></label>
 				</div>
-	              <thead>
-	              <tr>
-	                  <th><i class="fa fa-sort-numeric-asc"></i> Total Prestamos</th>
-	                  <th><i class="fa fa-money"></i> Monto</th>
+	            <thead>
+	              <tr style="color: #eeeeee;background-color: #517fa4">
+	                <th><i class="fa fa-sort-numeric-asc"></i> Total Prestamos</th>
+		           	<th><i class="fa fa-money"></i> Monto</th>
 	              </tr>
-	              </thead>
-	              <tbody>
-	              	<tr>
-		                <td style="padding-left:60px"><h4>{{$totalLoans}}</h4></td>
-		                <td><h4><strong>Q{{number_format($totalMontly, 2, '.', ',')}}</strong></h4></td>
-	              	</tr>
-	           	</tbody>
-	          </table>
-	      </div><!-- /content-panel -->
-	  </div><!-- /col-md-4 -->
-	  <div class="col-md-4"></div>
-	</div><!-- /row -->
-	<div class="row mt">
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
-			
-				<div class="col-md-3">
-					<form method="post" id="download" action="/reporte/download">
-						<input type="hidden" id="action" name="action" value="consolidado">
-						<button class="btn btn-warning" type="submit" form="download">
-							<i class="fa fa-download"></i>
-							PDF
-						</button>
-					</form>
-				</div>
-				<div class="col-md-3">
-					<form method="post" id="aplicarPago" action="/reporte/apply">
-						<input type="hidden" id="action" name="action" value="consolidado">
-						<button class="btn btn-success" type="submit" form="aplicarPago">
-							<i class="fa fa-sign-out"></i>
-							Aplicar Pago
-						</button>
-					</form>
-				</div>
-			
-		</div>
-		<div class="col-md-4"></div>
+	            </thead>
+	            <tbody>
+	            	<tr>
+	            		<td class="text-center"><h4>{{$totalLoans}}</h4></td>
+			            <td class="text-center"><h4><strong>Q{{number_format($totalMontly, 2, '.', ',')}}</strong></h4></td>
+	            	</tr>
+	            </tbody>
+	       	</table>
+	       	<div>
+				<form method="post" id="aplicarPago" action="/reporte/apply">
+					<input type="hidden" id="action" name="action" value="consolidado">
+					<button class="btn btn-success btn-custom waves-effect waves-light" type="submit" form="aplicarPago">
+						<i class="fa fa-sign-out"></i>
+						Aplicar Pago
+					</button>
+				</form>
+			</div>
+	   	</div>
 	</div>
-</section>
+	<div class="col-sm-4"></div>
+</div>
 @stop
